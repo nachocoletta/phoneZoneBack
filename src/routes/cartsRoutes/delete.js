9f902a55f1b509548e8a46054d45372994569d76
Nacho <ignacio_coletta@hotmail.com>
2023-04-16 23:39:34 -0300

    borro todo de guapo

--- src/routes/cartsRoutes/delete.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require ("express");
-const router = Router();
-
-const { deleteProductCart } = require('../../controllers/cartController');
-
-router.delete('/', deleteProductCart)
-
-module.exports = router
--- src/routes/cartsRoutes/update.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require ("express");
-const router = Router();
-
-const { updateStockProduct } = require('../../controllers/cartController');
-
-router.put('/', updateStockProduct);
-
-
-module.exports = router
