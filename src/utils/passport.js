095a8172f75b5011f74b4706f4832da35e357957
jooseariias <arijoose@gmail.com>
2023-04-20 08:58:34 -0300

    asdasda

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -2,16 +2,21 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const passport = require('passport');
 
 passport.use(new GoogleStrategy({
-  clientID: "1042216557034-foecoc9icuqhahh5bndoabjgukv7aa16.apps.googleusercontent.com",
-  clientSecret: "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",
-  callbackURL: "/auth/google/callback",
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "https://phonezoneback-production.up.railway.app/auth/google/callback",
   scope: ["profile", "email"],
   //passReqToCallback: true,
 },
 
-function (accessToken, refreshToken, profile, callback) {
-  callback(null, profile);
-  
+async function (accessToken, refreshToken, profile, callback) {
+  try {
+    // Perform additional processing here, such as creating a new user in your database
+    // based on the Google profile information
+    callback(null, profile);
+  } catch (error) {
+    callback(error, null);
+  }
 }));
 
 passport.serializeUser(function(user, done) {
