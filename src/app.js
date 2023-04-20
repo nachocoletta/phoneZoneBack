f38c162e17a25f7c9ff2843f8bf4dadb5fab1705
jooseariias <arijoose@gmail.com>
2023-04-20 08:29:53 -0300

    asdasda

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ require('../src/routes/auth.js');
 const server = express();
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://prueba-sad-git-main-jooseariias.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
