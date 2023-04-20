8dd5b1cbedb2e054ee3bdb76fb80df06ee23f143
jooseariias <arijoose@gmail.com>
2023-04-20 08:14:12 -0300

    asdasda

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const { User } = require("../db");
 const passport = require("passport");
 // const CLIENT = "https://front-phone-zone.vercel.app";
 // const CLIENT = "https://front-phone-zone-git-main-nachocoletta.vercel.app/"
-const CLIENT = "https://www.ariastv.online/"
+const CLIENT = "https://prueba-sad-git-main-jooseariias.vercel.app/"
 
 require("../utils/passport");
 function isLoggedIn(req, res, next) {
