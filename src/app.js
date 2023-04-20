a44ddd9d6d26374e059b985258229eed5b283b57
jooseariias <arijoose@gmail.com>
2023-04-20 08:09:27 -0300

    asdasda

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const server = express();
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
+  // res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
