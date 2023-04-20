faad67e177c61b7e27fced323be45f868eecbb1b
jooseariias <arijoose@gmail.com>
2023-04-20 10:02:41 -0300

    vvvvv

--- src/app.js
+++ src/app.js
@@ -20,17 +20,20 @@ server.use(
 );
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin','*');
-
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-server.use(session({
+app.use(session({
   secret: 'secret',
   resave: false,
   saveUninitialized: false,
+  cookie: {
+    secure: true,
+    maxAge: 3600000 // 1 hour
+  }
 }));
 
 server.use(passport.initialize());
