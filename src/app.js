ab0b4f31a21df8b94efd64d101c1857a6acfb2a3
Nacho <ignacio_coletta@hotmail.com>
2023-04-20 01:01:22 -0300

    deploy 19/04/23

--- src/app.js
+++ src/app.js
@@ -28,7 +28,8 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://front-phone-zone.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://front-phone-zone-git-main-nachocoletta.vercel.app/');
+  // res.header('Access-Control-Allow-Origin', 'https://front-phone-zone.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
